343e5385829d9a35bb6da8cef1abb2a5960573aa
Maksim Shamihulau <maksim.shamihulau@gmail.com>
2020-05-22 23:02:22 +0300

    mark up filter

--- src/components/SelectCurrencyButton.vue
+++ src/components/SelectCurrencyButton.vue
@@ -1,22 +1,34 @@
 <template>
 <div>
   <button @click="$modal.show($options.name)" class="select-currency-button">{{ selectedCurrency }}</button>
- 
-  <v-modal :name="$options.name" width="400px" classes="modal">
-    <div>
-      <div class="px-4 py-5">
-        <h2 class="text-lg text-center font-semibold">Select Currency</h2>
-        <button class="float-right -mt-8 rounded-full hover:bg-gray-200" @click="$modal.hide($options.name)">
-          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 18 18"><path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"/></svg>
-        </button>
-      </div>
-      <hr />
-      <div class="px-6 py-5 text-center">
-         <label v-for="(currency, index) in currencies" :key="currency" class="inline-flex items-center" :class="{'ml-6': index > 0}">
-          <input type="radio" class="form-radio" v-model="selectedCurrency" :value="currency">
-          <span class="ml-2">{{ currency }}</span>
-        </label>
-      </div>
+  <v-modal :name="$options.name"  classes="modal" height="400px">
+      <div class="flex flex-col h-full px-8 py-5">
+        <header class="mb-10">
+          <h2 class="text-lg text-center font-semibold">Select Currency</h2>
+          <button class="float-right -mt-8 rounded-full hover:bg-gray-200" @click="closeModal">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 18 18"><path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"/></svg>
+          </button>
+        </header>
+        <!-- <hr> -->
+        <div class="flex-grow mb-0">
+          <div class="flex items-center mb-8">
+            <!--<img src="@/assets/search-icon.svg" class="-mr-6 z-10" /> -->
+            <input class="form-input block w-full search-input outline-none" v-model="searchQuery" placeholder="Search Currency">
+            <button class="-ml-8" @click="clearInput">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 18 18"><path fill="#a0aec0" d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"/></svg>
+            </button>
+          </div>
+          <div class="flex flex-wrap overflow-auto">
+            <label v-for="currency in getFilteredCurrencyList()" :key="currency.name" class="flex items-center w-1/3 mb-6">
+              <input type="radio" class="form-radio" v-model="selectedCurrency" :value="currency.name">
+              <span class="ml-2">{{ currency.name }}</span>
+            </label> 
+          </div>
+        </div>
+        <footer class="flex justify-end">
+          <button class="px-4 py-2 font-semibold text-lg text-gray-700 border-gray-500 hover:bg-gray-200 uppercase mr-4" @click="closeModal">Cancel</button>
+          <button class="px-4 py-2 font-semibold text-lg text-white bg-blue-500 hover:bg-blue-400 uppercase" @click="onSave">Save</button>
+        </footer>
     </div>
   </v-modal> 
    <!-- <Modal title="Select Currency" name="test">  
@@ -27,25 +39,49 @@
   </Modal> -->
 </div>
 </template>
-
 <script>
-//import Modal from './Modal.vue'
-
 export default {
   name: 'SelectCurrencyButton',
-  components: {
-  //  Modal
-  },
   data() {
     return {
-      currencies: ['USD', 'EUR', 'GBP', 'RUB'],
-      selectedCurrency: 'USD'
+      currencies: [
+        { name: 'USD' }, 
+        { name: 'EUR' }, 
+        { name: 'GBP' }, 
+        { name: 'RUB' }, 
+        
+      ],
+      selectedCurrency: 'USD',
+      searchQuery: ''
     }
   },
+  created() {
+    //this.populateCryptoList();
+    //console.log('after populateCryptoList()');
+  },
   methods: {
-    
+    async populateCryptoList() {
+      const response = await fetch('https://api.coingecko.com/api/v3/coins/list');
+      const list = await response.json();
+      if (list) {
+      //  this.cryptos = list;
+      }
+      //console.log('populate finished')
+    },
+    getFilteredCurrencyList() {
+      return this.currencies.filter(c => c.name.toLowerCase().includes(this.searchQuery));
+    },
+    clearInput() {
+      this.searchQuery = '';
+    },
+    onSave() {
+      // TODO: Save Crypto filter into app state so it will be available for all components
+      this.closeModal();
+    },
+    closeModal() {
+      this.$modal.hide(this.$options.name);
+    }
   }
-
 }
 </script>
 
@@ -58,10 +94,6 @@ export default {
   
   .select-currency-button:hover {
     @apply border-white text-white;
-  }
-  
-  .form-radio {
-    @apply form-radio w-6 h-6;
   }
 </style>
 
--- src/pages/offers/filters/CryptoFilter.vue
+++ src/pages/offers/filters/CryptoFilter.vue
@@ -1,8 +1,38 @@
 <template>
   <div>
-    <button class="crypto-filter">
+    <button @click="$modal.show($options.name)" class="crypto-filter">
       {{ selectedCrypto }}
     </button>
+    <v-modal :name="$options.name"  classes="modal" height="400px">
+      <div class="flex flex-col h-full px-8 py-5">
+        <header class="mb-10">
+          <h2 class="text-lg text-center font-semibold">Select Cryptocurrency</h2>
+          <button class="float-right -mt-8 rounded-full hover:bg-gray-200" @click="closeModal">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 18 18"><path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"/></svg>
+          </button>
+        </header>
+        <!-- <hr> -->
+        <div class="flex-grow mb-0">
+          <div class="flex items-center mb-8">
+            <!--<img src="@/assets/search-icon.svg" class="-mr-6 z-10" /> -->
+            <input class="form-input block w-full search-input outline-none" v-model="searchQuery" placeholder="Search Cryptocurrency">
+            <button class="-ml-8" @click="clearInput">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 18 18"><path fill="#a0aec0" d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"/></svg>
+            </button>
+          </div>
+          <div class="flex flex-wrap overflow-auto">
+            <label v-for="crypto in getFilteredCryptoList()" :key="crypto.name" class="flex items-center w-1/3 mb-6">
+              <input type="radio" class="form-radio" v-model="selectedCrypto" :value="crypto.name">
+              <span class="ml-2">{{ crypto.name }}</span>
+            </label> 
+          </div>
+        </div>
+        <footer class="flex justify-end">
+          <button class="px-4 py-2 font-semibold text-lg text-gray-700 border-gray-500 hover:bg-gray-200 uppercase mr-4" @click="closeModal">Cancel</button>
+          <button class="px-4 py-2 font-semibold text-lg text-white bg-blue-500 hover:bg-blue-400 uppercase" @click="onSave">Save</button>
+        </footer>
+    </div>
+  </v-modal> 
   </div>
 </template>
 
@@ -12,12 +42,45 @@ export default {
   name: 'CryptoFilter',
   data() {
     return {
-      cryptos: ['BTC', 'ETH', 'XRP', 'EOS', 'USDT'],
-      selectedCrypto: 'Bitcoin'
+      cryptos: [
+        { name: 'Bitcoin' }, 
+        { name: 'Ethereum' }, 
+        { name: 'XRP' }, 
+        { name: 'Tether' }, 
+        { name: 'Bitcoin Cash' }, 
+        { name: 'Litecoin' }, 
+        { name: 'Binance Coin' }
+      ],
+      selectedCrypto: 'Bitcoin',
+      searchQuery: ''
     }
   },
+  created() {
+    //this.populateCryptoList();
+    //console.log('after populateCryptoList()');
+  },
   methods: {
-    
+    async populateCryptoList() {
+      const response = await fetch('https://api.coingecko.com/api/v3/coins/list');
+      const list = await response.json();
+      if (list) {
+      //  this.cryptos = list;
+      }
+      //console.log('populate finished')
+    },
+    getFilteredCryptoList() {
+      return this.cryptos.filter(c => c.name.toLowerCase().includes(this.searchQuery));
+    },
+    clearInput() {
+      this.searchQuery = '';
+    },
+    onSave() {
+      // TODO: Save Crypto filter into app state so it will be available for all components
+      this.closeModal();
+    },
+    closeModal() {
+      this.$modal.hide(this.$options.name);
+    }
   }
 }
 </script>
