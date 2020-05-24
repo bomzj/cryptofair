7cd1c91c21557f6f4190b687f1fb5dbd26add54f
Maksim Shamihulau <maksim.shamihulau@gmail.com>
2020-05-24 23:31:04 +0300

    resize offers list to fit xl screens

--- src/pages/offers/Offers.vue
+++ src/pages/offers/Offers.vue
@@ -7,13 +7,13 @@
       <p class="info-hint">Rating <span class="info-value">{{ offer.trader.rating }}</span>, Trades <span class="info-value">{{ offer.trader.trades }}</span></p>
       <p class="info-hint">Exchange <span class="info-value">{{ offer.exchange.name }}</span></p>
     </div>
-    <div class="px-6 py-4 w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4">
+    <div class="px-6 py-4 w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-2/6">
       <p class="info-hint mb-2">Trading Amount <span class="info-value inline-block">{{ formatTradingAmount(offer) }}</span></p>
       <div class="flex flex-row items-center" >
         <div class="info-hint whitespace-no-wrap mr-2">Pay by</div><img v-for="method in offer.paymentMethods" :key="method" width="36" height="36" class="mr-1" :src="getPaymentLogoUrl(method)"/>
       </div>
     </div>
-    <div class="px-6 py-4 whitespace-no-wrap leading-5 w-full sm:w-1/2 md:w-1/2 lg:w-1/4  xl:w-1/4">
+    <div class="px-6 py-4 whitespace-no-wrap leading-5 w-full sm:w-1/2 md:w-1/2 lg:w-1/4  xl:w-2/6">
       <div class="flex flex-col">
         <p class="mb-2 text-2xl font-medium">{{ formatConvertedPrice(offer) }} <span class="info-hint font-normal">per coin</span></p> 
         <p class="info-hint">Original Price <span class="info-value">{{ formatOriginalPrice(offer) }}</span></p>
