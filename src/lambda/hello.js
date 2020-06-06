7794a3ce685c5d27e927ad7637aa215f7aeb8563
Maksim Shamihulau <maksim.shamihulau@gmail.com>
2020-06-06 22:35:37 +0300

    integrate netlify lambda functions for microservices architecture

---
+++ src/lambda/hello.js
@@ -0,0 +1,7 @@
+exports.handler = function handler(event, context, callback) {
+  console.log(event)
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({msg: "Hello, World!"})
+  })
+}
