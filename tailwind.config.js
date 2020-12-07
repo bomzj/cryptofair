ea47f05de81268a4321e186108277f1dae8a2045
Maksim Shamihulau <maksim.shamihulau@gmail.com>
2020-12-07 21:51:44 +0300

    fix tailwind container not to ignore responsive breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,22 @@ module.exports = {
     boxShadow: false,
   },
   theme: {
-    // boxShadow: {
-    //   default: '0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06)',
-    //   md: '0 4px 6px -1px rgba(0, 0, 0, .1), 0 2px 4px -1px rgba(0, 0, 0, .06)',
-    //   lg: '0 10px 15px -3px rgba(0, 0, 0, .1), 0 4px 6px -2px rgba(0, 0, 0, .05)',
-    //   xl: '0 20px 25px -5px rgba(0, 0, 0, .1), 0 10px 10px -5px rgba(0, 0, 0, .04)',
-    //   '2xl': '0 25px 50px -12px rgba(0, 0, 0, .25)',
-    //   '3xl': '0 35px 60px -15px rgba(0, 0, 0, .3)',
-    //   inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
-    //   outline: '0 0 0 3px rgba(66, 153, 225, 0.5)',
-    //   focus: 'none',
-    //   'none': 'none',
-    // }
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [
     require('@tailwindcss/custom-forms'),
