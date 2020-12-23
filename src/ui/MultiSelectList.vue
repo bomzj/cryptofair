44e9a932aef6409b6bd100bc54abee13660a8629
Maksim Shamihulau <maksim.shamihulau@gmail.com>
2020-12-23 19:59:00 +0300

    add 4 column for select lists at XL responsive breakpoint

--- src/ui/MultiSelectList.vue
+++ src/ui/MultiSelectList.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col sm:flex-row sm:flex-wrap flex-grow content-start overflow-auto px-8 py-5">
     <label v-for="item in itemList" 
            :key="item" 
-           class="flex items-center sm:w-1/2 lg:w-1/3 mb-6 lg:mb-8 2xl:mb-10">
+           class="flex items-center sm:w-1/2 lg:w-1/3 2xl:w-1/4 mb-6">
       <input type="checkbox" 
               class="form-checkbox" 
               v-model="selected" 
