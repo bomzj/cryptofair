ebdbf7fb46503fd152553eeb9f7341ef45cd4ed4
Maksim Shamihulau <maksim.shamihulau@gmail.com>
2020-12-28 11:05:43 +0300

    increase Write to use height for mobile devices

--- src/layout/Footer.vue
+++ src/layout/Footer.vue
@@ -1,19 +1,25 @@
 <template>
   <footer class="m-8">
-    <div class="flex flex-col items-center ">
-      <h1 class="text-4xl font-medium text-center text-gray-700">
+    <div class="flex flex-col">
+      <h1 class="text-2xl font-medium text-center text-gray-700">
         All the best cryptocurrency prices directly from real people all around the world in one place!
         <!-- The best cryptocurrency prices to buy or sell Bitcoin, Ethereum 
         and other crypto directly from real people around the world in one place! -->
       </h1>
-      <ul class="list-none text-xl text-gray-600 mt-4">
-        <li>First and the only one p2p crypto exchange aggregator!</li>
-        <li>Looks up in leading p2p exchanges: LocalBitcoins, Paxful, LocalCryptos and etc</li>
-        <li>Finds the best prices to buy or sell Bitcoin, Ethereum and other crypto coins</li>
-        <li >Modern and easy to use UI with convenient search filters to see prices in your local currency</li>
-        <li>No fees, no registration, no app downloading</li>
-        <li>No verification, no KYC, no AML</li>
-      </ul>
+      <div class="flex flex-wrap flex-col lg:flex-row mt-4">
+        <ul class="list-none text-lg text-gray-600 w-full lg:w-2/3">
+          <li>First and the only one p2p crypto exchange aggregator!</li>
+          <li>Looks up in leading p2p exchanges: LocalBitcoins, Paxful, LocalCryptos and etc</li>
+          <li>Finds the best prices to buy or sell Bitcoin, Ethereum and other crypto coins</li>
+          <li >Modern and easy to use UI with convenient search filters to see prices in your local currency</li>
+          <li>No fees, no registration, no app downloading</li>
+          <li>No verification, no KYC, no AML</li>
+        </ul>
+        <div class="flex flex-col w-full lg:w-1/3">
+          <textarea class="form-textarea block flex-grow text-xl my-2" rows="6" placeholder="Write to us" />
+          <button class="button button-outline-primary self-end" click="submitFeedback">Submit</button>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
@@ -27,6 +33,6 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 ul li {
-  @apply my-2;
+  @apply my-4;
 }
 </style>
