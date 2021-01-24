53a82fac39d9d5f9cd0fa67ff69638c695aa5a0b
Maksim Shamihulau <maksim.shamihulau@gmail.com>
2021-01-24 23:00:47 +0300

    fix some typos

--- src/layout/Footer.vue
+++ src/layout/Footer.vue
@@ -1,18 +1,16 @@
 <template>
   <footer class="mt-20 px-6 mb-20">
     <h3 class="page-title text-center mb-4">
-        All Best Cryptocurrency Prices Directly From Millions Real People All Around The World In One Place! 
-        <!-- The best cryptocurrency prices to buy or sell Bitcoin, Ethereum 
-        and other crypto directly from real people around the world in one place! -->
+        All the Best Cryptocurrency Prices Directly From Millions Real People All Around The World In One Place! 
     </h3>
     <div class="flex flex-wrap">
       <div class="feature-list">
         <h4>First and the only one p2p crypto exchanges aggregator!</h4>
-        <h4>Compare prices among 6+ leading secure p2p exchanges: LocalBitcoins, Paxful, LocalCryptos, etc</h4>
-        <h4>Search for your best crypto traiding deal today without fees, registration, app downloading</h4>
+        <h4>Compare prices among 5 leading secure p2p exchanges: LocalBitcoins, Paxful, LocalCryptos, etc</h4>
+        <h4>Search for your best crypto trading deal today without fees, registration, app downloading</h4>
       </div>
       <div class="feature-list">
-        <h4>Find best prices to buy/sell the most popular cryptocoins - Bitcoin, Ethereum, XRP and others</h4>
+        <h4>Find the best prices to buy/sell the most popular cryptocoins - Bitcoin, Ethereum, Tether and others</h4>
         <h4>Modern and easy to use UI with convenient search filters to see prices in your local currency</h4>
         <h4>No verification, no KYC, no AML to start search now</h4>
       </div>
