8f1894673b134fc6e4775717926df989b93008a2
Maksim Shamihulau <maksim.shamihulau@gmail.com>
2021-01-27 17:10:18 +0300

    change promo image for social networks

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
       filename: 'index.html',
       // when using title option,
       // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
-      title: 'P2P Crypto Exchanges Aggregator ₿  (Best Prices 2021)',
+      title: 'P2P Crypto Exchanges Aggregator ₿ (Best Prices 2021)',
       // chunks to include on this page, by default includes
       // extracted common chunks and vendor chunks.
       chunks: ['chunk-vendors', 'chunk-common', 'offers']
